d0b474187de81777894fad8564c161974c01d267
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2025-02-01 13:20:57 +0200

    fix: siwe auth verification

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 import { Public } from './guards/public.decorator';
-import { AuthService } from './guards/auth.service';
+import { AuthService } from './auth.service';
 import { VerifyDto } from './dtos/verify.dto';
 
 @Public()
