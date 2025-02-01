d4c756a867e36d0218b74c1a26019796ed390724
MrJeleika <den932005@gmail.com>
2025-02-01 14:56:33 +0200

    wip: game page layout

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          'flex h-14 w-full rounded-[6px] bg-dark-gray px-5 py-4 text-[20px] transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-white/50 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-14 w-full rounded-[6px] bg-dark-gray px-5 py-4 text-[20px] text-white transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-white/50 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
