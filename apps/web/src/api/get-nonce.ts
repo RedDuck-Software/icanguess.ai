38fa0e998b616fe90fae37fb990ba33ad9e1a5b7
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2025-02-01 16:22:08 +0200

    fix: remove env

--- apps/web/src/api/get-nonce.ts
+++ apps/web/src/api/get-nonce.ts
@@ -6,5 +6,5 @@ export const getNonce = async (wallet?: string): Promise<string> => {
   const { data } = await publicClient.post(NONCE_URL, { wallet });
   console.log('data ==>', data);
 
-  return data;
+  return data.nonce;
 };
