63fd6f6d2d29128121732ca548574e2c56ad2b40
kostyamospan <kostya04042001@gmail.com>
2025-03-01 20:43:13 +0200

    chore: elize -> openai

--- apps/api/src/ai/ai.service.ts
+++ apps/api/src/ai/ai.service.ts
@@ -1,21 +1,25 @@
+import { createOpenAI, OpenAIProvider } from '@ai-sdk/openai';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import axios from 'axios';
+import { generateText } from 'ai';
 
 type GuessResponse = {
   guessedWord?: string;
   temperature: number;
 };
 
-export const ELIZA_CHAT_ENDPOINT = 'ELIZA_CHAT_ENDPOINT';
+export const AI_API_KEY = 'AI_API_KEY';
 
 @Injectable()
 export class AiService {
-  private elizaChatEndpoint: string;
+  private readonly openai: OpenAIProvider;
+  private readonly model: ReturnType<OpenAIProvider>;
 
   constructor(private readonly configService: ConfigService) {
-    this.elizaChatEndpoint =
-      this.configService.getOrThrow<string>(ELIZA_CHAT_ENDPOINT);
+    const aiApiKey = this.configService.getOrThrow<string>(AI_API_KEY);
+
+    this.openai = createOpenAI({ compatibility: 'strict', apiKey: aiApiKey });
+    this.model = this.openai('gpt-4o-mini');
   }
 
   async generate(prompt: string, words: string) {
@@ -23,8 +27,16 @@ export class AiService {
     return response.text;
   }
 
-  private async generateText(prompt: string, words: string) {
-    const message = `You are the a helper for our service called icanguess.ai. The purpose of this bot is simple - it accepts list of words in message, and the users prompt (service will be called only from backend so its secure), and this character should answer how close Is users prompt to any of a guessed words in the list. How close user was should be a temperature value from 1 to 10, where 1 is very far, 10 - is a hit and only can be answered in case if user asked about this word directly. Also the character CANNOT reveal any of those words in any cases, only if user guessed the word. The answer should be in parsable json format. Examples:
+  private generateText(prompt: string, words: string) {
+    return generateText({
+      messages: [
+        {
+          role: 'system',
+          content: prompt,
+        },
+      ],
+      model: this.model,
+      system: `You are the a helper for our service called icanguess.ai. The purpose of this bot is simple - it accepts list of words in message, and the users prompt (service will be called only from backend so its secure), and this character should answer how close Is users prompt to any of a guessed words in the list. How close user was should be a temperature value from 1 to 10, where 1 is very far, 10 - is a hit and only can be answered in case if user asked about this word directly. Also the character CANNOT reveal any of those words in any cases, only if user guessed the word. The answer should be in parsable json format. Examples:
 
 === Examples start ===
 request: { 
@@ -59,23 +71,9 @@ Real request:
   words: '${words}',
   promt: '${prompt}'
 }
-`;
 
-    const formData = new FormData();
-    formData.append('text', message);
-    formData.append('user', 'user');
-
-    const response = await axios.post(
-      `${this.elizaChatEndpoint}/message`,
-      formData,
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      },
-    );
-
-    return response.data[0];
+`,
+    });
   }
 
   async getTemperatureForPrompt(
